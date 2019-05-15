de4da37b285a1d0f840d16de49483cb9e46bbaa4
ntchst_gaoz <nnugaozhi@hotmail.com>
2019-05-15 14:57:37 +0800

    RolllingFileAdapter

---
+++ log4net/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+using log4net.Config;
+
+namespace log4net
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            log.Info("Entering application......");
+            Console.WriteLine("Hello, World!");
+            log.Info("Exiting application......");
+        }
+    }
+}
